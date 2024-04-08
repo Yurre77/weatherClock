fbf0595bc6a289961a8dd7dd1fa460a760f16139
Yurre <yurrr.schram@gmail.com>
2024-04-08 19:50:23 +0200

    time stuff should work

--- weatherClock/weatherClock.ino
+++ weatherClock/weatherClock.ino
@@ -1,4 +1,7 @@
 #include <DHT.h>
+#include <ESP8266WiFi.h>
+#include <WiFiManager.h>
+#include <NTPClient.h>
 #include <string.h>
 #include <U8g2lib.h>
 
@@ -6,6 +9,18 @@
 #define DHTPIN D4
 //Define constants
 #define FONTSIZE 8
+//change to a NTP server in your region
+#define NTPSERVER "pool.ntp.org"
+
+// static IP definitions
+IPAddress staticIP(192, 168, 34, 34);
+IPAddress gateway(145, 3, 249, 94);
+IPAddress subnet(255, 255, 255, 0);
+IPAddress dns(8, 8, 8, 8);
+
+// Time constructors
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP, NTPSERVER);
 
 //Constructors
 U8G2_SSD1306_128X64_NONAME_F_SW_I2C u8g2(U8G2_R0, /* clock=*/ SCL, /* data=*/ SDA, /* reset=*/ U8X8_PIN_NONE);
@@ -15,7 +30,7 @@ void setup() {
   Serial.begin(9600);
   dht.begin();
   u8g2.begin();
-
+  connectWiFi();
   u8g2.setFont(u8g2_font_logisoso16_tf);
   Serial.println("Setup complete");
 }
@@ -74,6 +89,31 @@ void drawSensor(float temp, float hum) {
 void drawTime() {
   int screenWidth = u8g2.getDisplayWidth();
   int screenHeight = u8g2.getDisplayHeight();
-  
-  u8g2.drawStr(0,((screenHeight/2)-FONTSIZE), "12:50");
+
+  String time = String(timeClient.getHours()) + ":" + String(timeClient.getMinutes());
+
+  u8g2.drawStr(FONTSIZE,((screenHeight/2)-FONTSIZE), time.c_str());
+}
+
+void connectWiFi(){
+  // configure the WiFi mode (default = STA + AP)
+  WiFi.mode(WIFI_STA);
+  WiFi.config(staticIP, subnet, gateway, dns);
+  WiFi.hostname("WeatherClock");
+
+  // WiFi manager, local initialisation, it is unneeded after it does it's thing
+  WiFiManager wm;
+
+  //uncomment the following line to wipe saved saved credentials
+  //wm.resetSettings();
+
+  // automatically connect using saved credentials
+  // if the connection fails, start an accesspoint to configure new credentials
+  bool res;
+  res = wm.autoConnect("weatherClockAP");
+  if(!res){
+    Serial.println("Failed to connect");
+  } else {
+    Serial.println("Connected... https://youtu.be/JPgjTU5U52c");
+  }
 }
