df5fecdf545b20e5aa8cf0bcc9f0fc544fddbe5c
Yurre <yurrr.schram@gmail.com>
2024-04-08 17:42:11 +0200

    managed to draw borders on the OLED

--- weatherClock/weatherClock.ino
+++ weatherClock/weatherClock.ino
@@ -5,10 +5,11 @@
 #define DHTPIN = D4;
 
 //Constructors
-U8G2_SSD1306_128X64_NONAME_1_SW_I2C u8g2(U8G2_R0, /* clock=*/ SCL, /* data=*/ SDA, /* reset=*/ U8X8_PIN_NONE);
+U8G2_SSD1306_128X64_NONAME_F_SW_I2C u8g2(U8G2_R0, /* clock=*/ SCL, /* data=*/ SDA, /* reset=*/ U8X8_PIN_NONE);
 
 void setup() {
   Serial.begin(9600);
+  sht.begin();
   u8g2.begin();
   Serial.println("Setup complete");
 }
@@ -16,8 +17,22 @@ void setup() {
 void loop() {
   drawBorders();
 
-  delay(1000);
+  u8g2.sendBuffer();
+  delay(10000);
   u8g2.clear();
+}
+
+void readDHT(float* humidity, float* temperature) {
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+
+  if (!isnan(h) && !isnan(t)) {
+    *humidity = h;
+    *temperature = t;
+  } else {
+    *humidity = -1;
+    *temperature = -1;
+  }
 }
 
 void drawBorders() {
@@ -26,6 +41,8 @@ void drawBorders() {
 
   Serial.printf("Width: %i, Height %i\n", screenWidth, screenHeight);
 
-  u8g2.drawRFrame(64, 32, 32, 16, 5);
+  u8g2.drawRFrame(0, 0, (screenWidth/2), screenHeight, 2);
+  u8g2.drawRFrame((screenWidth/2), 0, (screenWidth/2), (screenHeight/2), 2);
+  u8g2.drawRFrame((screenWidth/2), (screenHeight/2), (screenWidth/2), (screenHeight/2), 2);
   Serial.println("drawn borders");
 }
